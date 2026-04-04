f6ed72f3d6e9642ba5ef9ccfbdab88e7f7abe550
madshrped <madshrped@gmail.com>
2026-04-04 19:08:45 +0200

    ADD: Ly upgrade

--- system/modules/ly/ly.nix
+++ system/modules/ly/ly.nix
@@ -1,19 +1,19 @@
 {
+  environment.etc."ly/animations/current.dur".source = ./animations/current.cur;
   services.displayManager.ly = {
     enable = true;
     settings = {
-      animate = true;
-      animation = "gameoflife";
+      animation = "dur_file";
+      box_title = "MHRP PC -- NIXOS";
+      animation_frame_delay = 5;
+      full_color = true;
       bigclock = true;
       bigclock_24hr = true;
       auth_fails = 5;
-      auto_login_session = "Niri";
-      #auto_login_user = "mads";
+      auto_login_session = "niri";
       clear_password = true;
       default_input = "password";
-      gameoflife_entropy_interval = 10;
-      gameoflife_frame_delay = 6;
-      gameoflife_initial_density = 0.4;
+      dur_file_path = "/etc/ly/animations/current.dur";
     };
   };
 }
