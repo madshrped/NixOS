63c0f85fc0a8d1a06e29c3b6caea54fef18fa9d0
madshrped <madshrped@gmail.com>
2026-07-11 21:46:48 +0200

    EDIT: Priting fixed

--- system/modules/printing.nix
+++ system/modules/printing.nix
@@ -1,17 +1,34 @@
 { pkgs, ... }:
 {
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "Canon_TS5300_series";
+        location = "Home";
+        deviceUri = "ipp://325903000000.local:631/ipp/print";
+        model = "everywhere";
+      }
+    ];
+  };
+
   services = {
     printing = {
       enable = true;
       drivers = with pkgs; [
         gutenprint
         gutenprint-bin
+        cnijfilter2
       ];
+      browsing = false;
     };
     avahi = {
       enable = true;
       nssmdns4 = true;
       openFirewall = true;
     };
+  };
+  systemd.services.ensure-printers = {
+    after = [ "avahi-daemon.service" ];
+    wants = [ "avahi-daemon.service" ];
   };
 }
